e7c316c08c16f483222f828cdf08ed4d390dfd5a
ZHENG HAO <hzhe111@163.com>
2024-02-05 10:47:49 -0500

    add drap and resize to calendar

--- src/components/Doctor/Appointments/Appointments.jsx
+++ src/components/Doctor/Appointments/Appointments.jsx
@@ -3,10 +3,12 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import DashboardLayout from '../DashboardLayout/DashboardLayout';
 import { Modal, Input } from 'antd';
-
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 
 const localizer = momentLocalizer(moment);
+const DraggableCalendar = withDragAndDrop(Calendar);
 
 const Appointments = () => {
     const [visible, setVisible] = useState(false);
@@ -24,14 +26,24 @@ const Appointments = () => {
             id: 1,
             start: new Date(2024, 2, 5, 9, 0, 0),
             end: new Date(2024, 2, 5, 10, 0, 0),
-            title: 'Appointment for Mr. X'
+            title: 'Appointment for Mr. X',
+            content: 'meeting with Mr. X for his regular checkup.'
         },
         {
             id: 2,
             start: new Date(2024, 1, 6, 11, 0, 0),
-            end: new Date(2024, 1, 6, 12, 0, 0),
-            title: 'Appointment for Mrs. Y'
+            end: new Date(2024, 1, 6, 13, 0, 0),
+            title: 'Appointment for Mr. Zhou',
+            content: 'meeting with Miss Zhou for her checkup.'
         },
+        // more mock events
+        {
+            id: 3,
+            start: new Date(2024, 1, 11, 13, 0, 0),
+            end: new Date(2024, 1, 11, 15, 0, 0),
+            title: 'Appointment for Mrs. Wang',
+            content: 'meeting with Wang for cancer regular checkup.'
+        }
     ]);
 
     const handleSelectSlot = slotInfo => {
@@ -51,12 +63,15 @@ const Appointments = () => {
             title: `Appointment for ${appointmentData.patient}`,
             start: selectedSlot.start,
             end: selectedSlot.end,
+            content: appointmentData.appointmentContent,
+            note: appointmentData.note,
             allDay: false
         };
         setEvents([...events, newEvent]);
         setAppointmentData({
             patient: "",
             appointmentContent: "",
+            content: appointmentData.appointmentContent,
             note: ""
         });
         setNewEventModalVisible(false);
@@ -74,9 +89,30 @@ const Appointments = () => {
         });
     };
 
+    // Drag and Resize Event Handlers
+    const onEventDrop = ({ event, start, end }) => {
+        const idx = events.indexOf(event);
+        let updatedEvent = { ...event, start, end };
+
+        let nextEvents = [...events];
+        nextEvents.splice(idx, 1, updatedEvent);
+
+        setEvents(nextEvents);
+    };
+
+    const onEventResize = ({ event, start, end }) => {
+        const idx = events.indexOf(event);
+        let updatedEvent = { ...event, start, end };
+
+        let nextEvents = [...events];
+        nextEvents.splice(idx, 1, updatedEvent);
+
+        setEvents(nextEvents);
+    };
+
     return (
         <DashboardLayout>
-            <Calendar
+             <DraggableCalendar
                 localizer={localizer}
                 defaultDate={new Date()}
                 defaultView="week"
@@ -84,9 +120,13 @@ const Appointments = () => {
                 style={{ height: "80vh" }}
                 onSelectSlot={handleSelectSlot}
                 onSelectEvent={handleSelectEvent}
+                onEventDrop={onEventDrop}
+                onEventResize={onEventResize}
+                resizable={true}
                 selectable={true}
             />
 
+            {/* Event Detail Modal */}
             <Modal title="Appointment Details" visible={visible} onCancel={handleCancel}>
                 <p>
                     <strong>Day:</strong> {selectedEvent.start && moment(selectedEvent.start).format('YYYY-MM-DD')}
@@ -95,10 +135,17 @@ const Appointments = () => {
                     <strong>Time:</strong> {selectedEvent.start && `${moment(selectedEvent.start).format('HH:mm')} - ${moment(selectedEvent.end).format('HH:mm')}`}
                 </p>
                 <p>
-                <strong>Patient:</strong> {selectedEvent.title?.split('for ')[1]}                
+                    <strong>Patient:</strong> {selectedEvent.title?.split('for ')[1]}                
+                </p>
+                <p>
+                    <strong>Appointment Content:</strong> {selectedEvent.content}
+                </p>
+                <p>
+                    <strong>Note:</strong> {selectedEvent.note}
                 </p>
             </Modal>
 
+            {/* New Evenet Modal */}
             <Modal title="Book an Appointment" visible={newEventModalVisible} onOk={handleOk} onCancel={handleCancel}>
                 <p>
                     <strong>Day:</strong> {selectedSlot.start && moment(selectedSlot.start).format('YYYY-MM-DD')}
