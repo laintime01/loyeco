56338d5854c3f56087acc8ac18c199bfd61f2dd9
ZHENG HAO <hzhe111@163.com>
2024-04-14 11:09:21 -0400

    remove clinic and license info in profile settings

--- src/components/Doctor/ProfileSetting/DoctorProfileSetting.jsx
+++ src/components/Doctor/ProfileSetting/DoctorProfileSetting.jsx
@@ -36,17 +36,12 @@ const DoctorProfileSetting = () => {
         setProfileData({ ...profileData, [name]: value });
     };
 
-    const handleLicenseChange = (index, e) => {
-        const updatedLicenses = [...profileData.licenses];
-        updatedLicenses[index] = { ...updatedLicenses[index], [e.target.name]: e.target.value };
-        setProfileData({ ...profileData, licenses: updatedLicenses });
-    };
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(profileData);
         try {
-            await updateProfile(profileData).unwrap();
+            const res = await updateProfile(profileData).unwrap();
+            console.log(res);
             message.success('Profile Updated Successfully');
             refetch();
         } catch (error) {
@@ -112,76 +107,6 @@ const DoctorProfileSetting = () => {
                             <input name="clinicName" value={profileData.clinicName} onChange={handleChange} className="form-control" />
                         </div>
                     </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic Location Name</label>
-                            <input name="clinicLocationName" value={profileData.clinicLocationName} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic Address</label>
-                            <input name="clinicAddress" value={profileData.clinicAddress} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic City</label>
-                            <input name="clinicCity" value={profileData.clinicCity} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic Province</label>
-                            <input name="clinicProvince" value={profileData.clinicProvince} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic Country</label>
-                            <input name="clinicCountry" value={profileData.clinicCountry} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic Postal Code</label>
-                            <input name="clinicPostalCode" value={profileData.clinicPostalCode} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic Phone</label>
-                            <input name="clinicPhone" value={profileData.clinicPhone} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="form-group mb-2 card-label">
-                            <label>Clinic Email</label>
-                            <input name="clinicEmail" value={profileData.clinicEmail} onChange={handleChange} className="form-control" />
-                        </div>
-                    </div>
-
-                    {/* Licenses Information */}
-                    {profileData.licenses.map((license, index) => (
-                        <div key={index} className="col-md-12">
-                            <div className="form-group mb-2 card-label">
-                                <label>License ID</label>
-                                <input name="licenseId" value={license.licenseId} onChange={(e) => handleLicenseChange(index, e)} className="form-control" />
-                            </div>
-                            <div className="form-group mb-2 card-label">
-                                <label>License Type</label>
-                                <input name="licenseType" value={license.licenseType} onChange={(e) => handleLicenseChange(index, e)} className="form-control" />
-                            </div>
-                        </div>
-                    ))}
 
                     <div className='text-center my-3'>
                         <Button htmlType='submit' type="primary" size='large'>
