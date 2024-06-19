acaa38dc90fbdb5388af4cfb15a80135e474ce00
ZHENG HAO <hzhe111@163.com>
2024-06-19 07:57:39 -0400

    fix react router bug

---
+++ src/utils/ProtectedRoute.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = () => {
+  const token = sessionStorage.getItem('accessToken');
+  return token ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
